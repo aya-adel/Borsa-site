5fe9b623bb118f3f36e6763ffddf2cfa0aa34e64
aya-adel <eng.ayaadel92@yahoo.com>
2016-03-04 03:22:18 +0200

    the login and save in database without validation yat + first table in dtabase borsa table data for users

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+include ('config.php');
+
+if (isset($_POST['reg'])&&isset($_POST['reg_name'])&&isset($_POST['reg_email'])&&isset($_POST['reg_pass'])&&isset($_POST['reg_repass'])) {
+    $link = mysqli_connect(
+            $config['server'], $config['username'], $config['passoword'], $config['database']);
+
+    if (!$link) {
+        echo " error :unable " . mysqli_connect_error();
+    }
+    
+    
+    $name=$_POST['reg_name'];
+    $email=$_POST['reg_email'];
+    $passwd=$_POST['reg_repass'];
+
+
+    $user = mysqli_query($link, "insert into data (name,email,passwd) values( '" .  $name . "' , '" . $email. "' ,'" . $passwd . "')");
+    if ($user) {
+        echo "l2ha ";
+    }
+
+
+
+ 
+}
+?>
