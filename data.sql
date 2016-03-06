9cf38893310de528f7b1969c2933308de6eb43c7
DevHanan <hanan.taha1993@gmail.com>
2016-03-06 19:31:28 +0200

    model of Database as dump file

--- data.sql
+++ data.sql
@@ -1,46 +1,103 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.47, for debian-linux-gnu (x86_64)
 --
--- Host: localhost
--- Generation Time: Mar 04, 2016 at 03:18 AM
--- Server version: 5.5.47-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.14
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: borsa
+-- ------------------------------------------------------
+-- Server version	5.5.47-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `borsa`
+-- Table structure for table `Share`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `Share`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Share` (
+  `id` int(200) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `price` varchar(200) NOT NULL,
+  `time` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=404 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Share`
+--
+
+LOCK TABLES `Share` WRITE;
+/*!40000 ALTER TABLE `Share` DISABLE KEYS */;
+INSERT INTO `Share` VALUES (365,'Apple Inc.','103.01','0000-00-00'),(384,'Symantec Corporation','16.62','0000-00-00'),(394,'Microsoft Corporation','52.03','0000-00-00'),(396,'Hewlett Packard Enterprise Comp','15.44','0000-00-00'),(397,'Petroleo Brasileiro S.A.- Petro','5.26','0000-00-00'),(398,'H&R Block, Inc. Common Stock','27.76','0000-00-00'),(399,'Alphabet Inc.','730.22','0000-00-00'),(401,'Seadrill Limited Ordinary Share','5.97','0000-00-00'),(402,'North Atlantic Drilling Ltd. Co','3.34','0000-00-00'),(403,'Whiting Petroleum Corporation C','7.09','0000-00-00');
+/*!40000 ALTER TABLE `Share` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `data`
 --
 
-CREATE TABLE IF NOT EXISTS `data` (
-  `id` int(50) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `data` (
+  `id` int(50) NOT NULL,
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `passwd` int(50) NOT NULL,
+  `passwd` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `data`
 --
 
-INSERT INTO `data` (`id`, `name`, `email`, `passwd`) VALUES
-(1, 'aya', 'adel', 111);
+LOCK TABLES `data` WRITE;
+/*!40000 ALTER TABLE `data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `data` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `user_share`
+--
+
+DROP TABLE IF EXISTS `user_share`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_share` (
+  `user_id` int(50) NOT NULL,
+  `share_id` int(50) NOT NULL,
+  `flag` int(10) NOT NULL,
+  `wPrice` int(50) NOT NULL,
+  PRIMARY KEY (`user_id`,`share_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_share`
+--
+
+LOCK TABLES `user_share` WRITE;
+/*!40000 ALTER TABLE `user_share` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_share` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-03-06 19:29:13
