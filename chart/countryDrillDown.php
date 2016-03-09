203016c283232680de5462ad8e504f572067afdb
DevHanan <hanan.taha1993@gmail.com>
2016-03-09 21:23:18 +0200

    inital chart of Share price by  Hanan &aya

---
+++ chart/countryDrillDown.php
@@ -0,0 +1,121 @@
+<?php
+   include("includes/fusioncharts.php");
+
+   $hostdb = "localhost";  // MySQl host
+   $userdb = "root";  // MySQL username
+   $passdb = "hggih;fv";  // MySQL password
+   $namedb = "borsa";  // MySQL database name
+
+   // Establish a connection to the database
+   $dbhandle = new mysqli($hostdb, $userdb, $passdb, $namedb);
+
+  /*Render an error message, to avoid abrupt failure, if the database connection parameters are incorrect */
+   if ($dbhandle->connect_error) {
+  	exit("There was an error with your connection: ".$dbhandle->connect_error);
+   }
+?>
+<html>
+   <head>
+  	<title> Column 2D Chart</title>
+  	<link  rel="stylesheet" type="text/css" href="css/style.css" />
+
+  	<script src="fusioncharts/fusioncharts.js"></script>
+   </head>
+   <body>
+  	<?php
+
+
+     	$cityQuery = "SELECT Name, price FROM Share";
+
+     	$cityPrepStmt = $dbhandle->prepare($cityQuery);
+
+     	if($cityPrepStmt === false) {
+        	exit("Error while preparing the query to fetch data from City Table. ".$dbhandle->error);
+     	}
+
+     	// Bind the parameters to the query prepared
+     	$cityPrepStmt->bind_param("s", $countryCode);
+
+     	// Execute the query
+     	$cityPrepStmt->execute();
+
+     	// Get the results from the query executed
+     	$cityResult = $cityPrepStmt->get_result();
+
+     	// If the query returns a valid response, prepare the JSON string
+     	if ($cityResult) {
+
+        	/* Form the SQL query that will return the country name based on the country code. The result of the above query contains only the country code. The country name is needed to be rendered as a caption for the chart that shows the 10 most populous cities */
+
+        	$countryNameQuery = "SELECT Name FROM Country WHERE Code = ?";
+
+        	// Prepare the query statement
+        	$countryPrepStmt = $dbhandle->prepare($countryNameQuery);
+
+        	// If there is an error in the statement, exit with an error message
+        	if($countryPrepStmt === false) {
+           	exit("Error while preparing the query to fetch data from Country Table. ".$dbhandle->error);
+        	}
+
+        	// Bind the parameters to the query prepared
+        	$countryPrepStmt->bind_param("s", $countryCode);
+
+        	// Execute the query
+        	$countryPrepStmt->execute();
+
+        	// Bind the country name to the variable `$countryName`
+        	$countryPrepStmt->bind_result($countryName);
+
+        	// Fetch the result from prepared statement
+        	$countryPrepStmt->fetch();
+
+        	// The `$arrData` array holds the chart attributes and data
+        	$arrData = array(
+                "chart" => array(
+                    "caption" => "Top 10 Most Populous Cities in ".$countryName,
+                    "paletteColors" => "#0075c2",
+                    "bgColor" => "#ffffff",
+                    "borderAlpha"=> "20",
+                    "canvasBorderAlpha"=> "0",
+                    "usePlotGradientColor"=> "0",
+                    "plotBorderAlpha"=> "10",
+                    "showXAxisLine"=> "1",
+                    "xAxisLineColor" => "#999999",
+                    "showValues"=> "0",
+                    "divlineColor" => "#999999",
+                    "divLineIsDashed" => "1",
+                    "showAlternateHGridColor" => "0"
+              	)
+           	);
+
+        	$arrData["data"] = array();
+
+	// Push the data into the array
+        	while($row = $cityResult->fetch_array()) {
+                array_push($arrData["data"], array(
+              	"label" => $row["Name"],
+              	"value" => $row["price"]
+              	)
+           	);
+        	}
+
+           /*JSON Encode the data to retrieve the string containing the JSON representation of the data in the array. */
+
+        	$jsonEncodedData = json_encode($arrData);
+
+      	  /*Create an object for the column chart using the FusionCharts PHP class constructor. Syntax for the constructor is `FusionCharts("type of chart", "unique chart id", "width of chart", "height of chart", "div id to render the chart", "data format", "data source")`.*/
+
+        	$columnChart = new FusionCharts("column2D", "myFirstChart" , 600, 300, "chart-1", "json", $jsonEncodedData);
+
+        	// Render the chart
+        	$columnChart->render();
+
+        	// Close the database connection
+        	$dbhandle->close();
+     	}
+  	?>
+
+  	<a href="country.php">Back</a>
+  	<div id="chart-1"><!-- Fusion Charts will render here--></div>
+   </body>
+</html>
--- stock.php
+++ stock.php
@@ -1,11 +1,11 @@
 <?php
 require_once('class.stockMarketAPI.php'); 
 $user="root";
-$password="admin";
+$password="hggih;fv";
 $host="localhost";
 $database="borsa";
-$con=mysqli_connect($host,$user,$password,$database);
-if($con->connect_errno)
+$link=mysqli_connect($host,$user,$password,$database);
+if($link->connect_errno)
 {
 	echo "Error:couldnot connect to database";
 	exit();
@@ -13,52 +13,60 @@ if($con->connect_errno)
 else
 {
 $StockMarketAPI = new StockMarketAPI();
-$StockMarketAPI->symbol = array('AAPL', 'MSFT', 'GOOGL','SYMC','HPE','PBR','HRB','SDRL','NADL','WLL');
+$StockMarketAPI->symbol = 
+array('AAPL', 'MSFT', 'GOOGL','SYMC','HPE','PBR','HRB','SDRL','NADL','WLL');
 $symbol_array = $StockMarketAPI->getData();
-
 $AAPL_Price=$symbol_array['AAPL']['price'];
 $AAPL_name=$symbol_array['AAPL']['name'];
-
+//echo $AAPL_name;
 $SYMC_Price =$symbol_array['SYMC']['price'];
 $SYMC_name=$symbol_array['SYMC']['name'];
-
+//echo $SYMC_name;
 $MSFT_price =$symbol_array['MSFT']['price'];
 $MSFT_name=$symbol_array['MSFT']['name'];
-
+//echo $MSFT_name;
 $HPE_price=$symbol_array['HPE']['price'];
 $HPE_name=$symbol_array['HPE']['name'];
-
+//echo $HPE_name;
 $PBR_price=$symbol_array['PBR']['price'];
-$PBR_name=$symbol_array['HPE']['name'];
-
+$PBR_name=$symbol_array['PBR']['name'];
+//echo $PBR_name;
 $HRB_price=$symbol_array['HRB']['price'];
 $HRB_name=$symbol_array['HRB']['name'];
-
+//echo $HRB_name;
+//echo "\n";
 $GOOGL_price=$symbol_array['GOOGL']['price'];
 $GOOGL_name=$symbol_array['GOOGL']['name'];
-
+echo $GOOGL_name;
+echo "\n";
 $SDRL_price=$symbol_array['SDRL']['price'];
 $SDRL_name=$symbol_array['SDRL']['name'];
-
+//echo $SDRL_name; 
+//echo "\n";
 $NADL_price=$symbol_array['NADL']['price'];
 $NADL_name=$symbol_array['NADL']['name'];
-
+//echo $NADL_name;
+//echo "\n";
 $WLL_price=$symbol_array['WLL']['price'];
 $WLL_name=$symbol_array['WLL']['name'];
+//echo $WLL_name;
+//echo "\n";
+
+  $date =  date("F j, Y, g:i a");  
+$query=mysqli_query($link,"update Share set price ='$AAPL_Price' , time='$date' where name='$AAPL_name'");
+$query=mysqli_query($link,"update Share set price ='$SYMC_Price',time='$date' where name='$SYMC_name'");
+$query=mysqli_query($link,"update Share set price ='$MSFT_price',time='$date' where name='$MSFT_name'");
+$query=mysqli_query($link,"update Share set price ='$HPE_price',time='$date' where name='$HPE_name'");
+$query=mysqli_query($link,"update Share set price ='$PBR_price',time='$date' where name='$PBR_name'");
+$query=mysqli_query($link,"update Share set price ='$HRB_price',time='$date' where name='$HRB_name'");
 
 
+$query=mysqli_query($link,"update Share set price ='$SDRL_price',time='$date' where name='$SDRL_name'");
+$query=mysqli_query($link,"update Share set price ='$NADL_price',time='$date' where name='$NADL_name'");
+$query=mysqli_query($link,"update Share set price ='$WLL_price',time='$date' where name='$WLL_name'");
 
-// update statement 
-$query=mysqli_query($link,"update share set price ='$AAPL_Price' where name='$AAPL_name'");
-$query=mysqli_query($link,"update share set price ='$SYMC_Price' where name='$SYMC_name'");
-$query=mysqli_query($link,"update share set price ='$MSFT_price' where name='$MSFT_name'");
-$query=mysqli_query($link,"update share set price ='$HPE_price' where name='$HPE_name'");
-$query=mysqli_query($link,"update share set price ='$PBR_price' where name='$PBR_name'");
-$query=mysqli_query($link,"update share set price ='$HRB_price' where name='$HRB_name'");
-$query=mysqli_query($link,"update share set price ='$SDRL_price' where name='$$SDRL_name'");
-$query=mysqli_query($link,"update share set price ='$NADL_price' where name='NADL_name'");
-$query=mysqli_query($link,"update share set price ='$WLL_price' where name='$WLL_name'");
-$query=mysqli_query($link,"update share set price ='$GOOGL_price' where name='$GOOGL_price'");
+
+$query=mysqli_query($link,"update Share set price ='$GOOGL_price',time='$date' where name='$GOOGL_name'");
 
 }
 
